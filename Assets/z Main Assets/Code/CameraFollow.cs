2063de755666d98904e42dc98e1f37c2a350df2d
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-26 19:26:46 -0400

    Added player movement and camera follow

---
+++ Assets/z Main Assets/Code/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    private void Update()
+    {
+        this.transform.position = player.transform.position + cameraOffset;
+    }
+}
---
+++ Assets/z Main Assets/Code/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+}
