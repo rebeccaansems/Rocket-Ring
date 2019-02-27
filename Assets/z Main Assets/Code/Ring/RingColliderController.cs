41abcd450ee93029043095eb45e8de97f90cb2a4
Rebecca Ansems <ansems_rebecca@hotmail.com>
2019-02-26 20:15:45 -0400

    Rings explode when player goes through them

---
+++ Assets/z Main Assets/Code/Ring/RingColliderController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingColliderController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ringParticles;
+
+    private void Start()
+    {
+        ringParticles.SetActive(true);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            ParticleSystem.MainModule particles = ringParticles.GetComponent<ParticleSystem>().main;
+            particles.startSpeed = 4;
+
+            StartCoroutine(DestroySystems(particles));
+        }
+    }
+
+    IEnumerator DestroySystems(ParticleSystem.MainModule particles)
+    {
+        Destroy(this.GetComponent<Collider>());
+
+        yield return new WaitForSeconds(0.1f);
+        particles.loop = false;
+    }
+
+}
